88619d058bb95fc5a99d5b02055821c75cfc2f0e
M MOHAMED IMRAN KHAN <99347671+imrankhan02@users.noreply.github.com>
2024-06-21 01:48:54 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[]args){
+    int a = 4;
+    int result = a == 4 ? 102:8;
+    System.out.println(result);
+    }
+}
---
+++ src/free.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class free {
+
+	/**
+	 * 
+	 */
+	public free() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
